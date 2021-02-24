2631b75b93901f6612999b092684e6bb236ee570
tuckerward20 <tuckerward20@gmail.com>
2021-02-24 14:58:26 -0600

    "completed project, no bonus"

---
+++ KanyeWest/GetQuote.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    public class GetQuote
+    {
+        public static string SwansonQuote()
+        {
+            var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var client = new HttpClient();
+
+            var swansonResponse = client.GetStringAsync(swansonURL).Result;
+
+            var swansonQuote = JArray.Parse(swansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return swansonQuote;
+        }
+
+        public static string KanyeQuote()
+        {
+            var kanyeURL = "https://api.kanye.rest";
+
+            var client = new HttpClient();
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+    }
+}
---
+++ KanyeWest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var swansonQuote = GetQuote.SwansonQuote();
+
+                var kanyeQuote = GetQuote.SwansonQuote();
+
+                Console.WriteLine($"Kanye says: {kanyeQuote}");
+
+                Console.WriteLine($"Ron says: {swansonQuote}");
+            }
+        }
+    }
+}
